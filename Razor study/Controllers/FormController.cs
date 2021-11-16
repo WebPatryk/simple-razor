5cfa54d75426644d95e73bbdaf0cf260d482b7c0
Patryk Lekki <patryk.lekki@awesome-ind.com>
2021-11-16 20:26:47 +0100

    Add project files.

---
+++ Razor study/Controllers/FormController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Razor_study.Models;
+
+
+
+namespace Razor_study.Controllers
+{
+    public class FormController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(FormModel model)
+        {
+            if (ModelState.Count>= 1 && ModelState.Count <=5)
+            {
+                return View("Created", model);
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Razor study/Models/FormModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Razor_study.Models
+{
+    public class FormModel
+    {
+        [Display(Name = "Twoje imię")]
+        [Required]
+        public string Name { get; set; }
+        [Display(Name = "Ilość powtórzeń")]
+        public int Count { get; set; }
+    }
+}
